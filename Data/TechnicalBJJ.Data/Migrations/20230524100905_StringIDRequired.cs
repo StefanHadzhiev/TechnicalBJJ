249ab23f3b43ce79b46723dc3547e1f57c8e8301
Stefan Hadzhiev <stefanhadzhiev24@gmail.com>
2023-05-31 00:08:46 +0300

    missing files

---
+++ Data/TechnicalBJJ.Data/Migrations/20230524100905_StringIDRequired.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechnicalBJJ.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StringIDRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Techniques_StartingPositions_StartingPositionId",
+                table: "Techniques");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartingPositionId",
+                table: "Techniques",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Techniques_StartingPositions_StartingPositionId",
+                table: "Techniques",
+                column: "StartingPositionId",
+                principalTable: "StartingPositions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Techniques_StartingPositions_StartingPositionId",
+                table: "Techniques");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartingPositionId",
+                table: "Techniques",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Techniques_StartingPositions_StartingPositionId",
+                table: "Techniques",
+                column: "StartingPositionId",
+                principalTable: "StartingPositions",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ Data/TechnicalBJJ.Data/Seeding/ApplicationUsersSeeder.cs
@@ -0,0 +1,31 @@
+﻿namespace TechnicalBJJ.Data.Seeding
+{
+    using Microsoft.AspNetCore.Identity;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using TechnicalBJJ.Data.Models;
+
+    public class ApplicationUsersSeeder : ISeeder
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            //if (dbContext.Users.Any(x => x.UserName == "admin@admin.com"))
+            //{
+            //    return;
+            //}
+
+            //// Create user and add him to role Administrator if he does not exist
+            //var user = new ApplicationUser();
+            //user.Email = "admin@admin.com";
+            //user.UserName = "admin@admin.com";
+            //user.EmailConfirmed = true;
+            //user.PhoneNumber = "12123123";
+
+            //await this.userManager.CreateAsync(user, "admin123");
+        }
+    }
+}
